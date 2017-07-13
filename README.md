97fe674aecc3388437b284e8c1f5c5001651efcc
tyj144 <tylerj81@gmail.com>
2017-07-13 15:46:39 -0400

    Added help feature and detailed README

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# bookmark-separator
-Generate a separatethis.com style title with your own title.
+# Bookmark Separator
+Generate a separatethis.com style title with a custom title.
+
+# Usage
+Requires pyperclip for the script to copy the title to your clipboard.
+
+'''
+python2.7 bookmark\_separator.py [-h] title
+'''
+
+Rename your bookmark to the copied string and use it as a separator.
+
+Example result: - - - - - - - - - - - - - - - - - - - - - webdev - - - - - - - - - - - - - - - - - - - - - 
+![Example usage of Bookmark Separator](https://github.com/tyj144/bookmark-separator/blob/master/example.png)
--- bookmark-separator.py
+++ bookmark-separator.py
@@ -1,8 +1,15 @@
-import sys, pyperclip
+import sys, pyperclip, argparse
 
+# Creates a separatethis.com style separator with a title
 def create_separator(title, style="-"):
-  dash = ((48 - len(title)) // 2 ) * (style + " ")
-  return dash + title + " " + dash
+  length = 48 - len(title) // 2 
+  start = length * (style + " ")
+  end = length * (" " + style)
+  return start + title + end
+
+parser = argparse.ArgumentParser(description='Create a bookmark separator.')
+parser.add_argument('title', metavar='title', type=str, help='title of bookmarks section')
+args = parser.parse_args()
 
 separator = create_separator(sys.argv[1])
 pyperclip.copy(separator)
